4cde9a2826f52f981863978525bea80d7a395222
yavar29 <yavarkhan1997@gmail.com>
2020-09-15 02:30:00 +0530

    add program linked_list.java

---
+++ linked_list/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        list.insert(6);
+        list.insert(24);
+        list.insert(52);
+        list.insert(64);
+        list.insert(33);
+
+        list.show();
+
+    }
+}
